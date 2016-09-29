72084da7f80e0fef611b488b963121e4b4944211
Patrick Johansen <Patrick Johansen>
2016-09-29 22:44:10 +0200

    First commit

---
+++ src/main/java/jsonconverter/IJSONConverter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsonconverter;
+
+import entity.City;
+import entity.Country;
+import java.util.List;
+
+/**
+ *
+ * @author xboxm
+ */
+public interface IJSONConverter {
+    
+    public String CityToJson(City c);
+    public String ListToJson(List list);
+    public City JsonToCity(String json);
+    public Country JsonToCountry(String json);
+    
+}
